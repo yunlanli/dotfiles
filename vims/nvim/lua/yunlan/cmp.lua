912eb6d06de2d20eead85203b9a9fdd648afc5ae
yunlanli <chnsidney@gmail.com>
2022-01-01 21:21:52 -0500

    [nvim] update colorizer and cmp config

--- vims/nvim/lua/yunlan/cmp.lua
+++ vims/nvim/lua/yunlan/cmp.lua
@@ -24,7 +24,7 @@ cmp.setup({
 	sources = cmp.config.sources{
 		{ name = 'nvim_lua' },
 		{ name = 'nvim_lsp' },
-		-- { name = 'path' },
+		{ name = 'path' },
 		{ name = 'ultisnips' },
 		{ name = 'buffer', keyword_length = 5 },
 	},
--- vims/nvim/lua/yunlan/colorizer.lua
+++ vims/nvim/lua/yunlan/colorizer.lua
@@ -1,3 +1,5 @@
+-- Mac's default terminal doesn't support truecolor
+-- Use iTerm on Mac OS instead
 vim.opt.termguicolors = true
 
 require('colorizer').setup()
