50a36a50dc5b57d8b73b0fe0390d52abb7e879d7
yunlanli <chnsidney@gmail.com>
2022-06-11 23:17:38 -0700

    [ emacs ] dump doom config

---
+++ doom/config.el
@@ -0,0 +1,103 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+
+(setq user-full-name "Yunlan Li"
+      user-mail-address "yl4387@columbia.edu")
+(setq doom-theme 'doom-one-light)
+(setq display-line-numbers-type 'relative)
+
+
+;;
+;; Key Bindings
+;;
+
+(map! :leader "f t" 'neotree)
+
+
+;;
+;; Org Mode
+;;
+
+(setq org-directory "~/Dropbox/Life/org")
+
+(after! org
+  (org-add-link-type
+   "color"
+   (lambda (path)
+     (message (concat "color "
+                      (progn (add-text-properties
+                              0 (length path)
+                              (list 'face `((t (:foreground ,path))))
+                              path) path))))
+   (lambda (path desc format)
+     (cond
+      ((eq format 'html)
+       (format "<span style=\"color:%s;\">%s</span>" path desc))
+      ((eq format 'latex)
+       (format "{\\color{%s}%s}" path desc))))))
+
+(add-hook 'org-mode-hook
+  (lambda ()
+    (plist-put org-format-latex-options :scale 0.7)))
+
+;;
+;; Olivetti
+;;
+
+(use-package olivetti
+  :init
+  (setq olivetti-body-width 100))
+
+
+;;
+;;  Org Roam
+;;
+
+(setq org-roam-directory "~/Dropbox/Study/org-roam")
+(require 'org-roam)
+(org-roam-db-autosync-mode)
+
+
+;;
+;; Org Capture
+;;
+
+(defun yl/find-create-date-entry ()
+  "Append to end of or create Org entry with date heading."
+  (let*
+      (
+       (dayInLastWeek (org-read-date nil t "-7d"))
+       (dayInNextWeek (org-read-date nil t "+7d"))
+       (thisMonday (org-read-date nil t "++Mon" nil dayInLastWeek))
+       (thisSunday (org-read-date nil t "--Sun" nil dayInNextWeek))
+       (heading (format "* %s" (format-time-string "%Y")))
+       (heading2 (format "** %s (Monday %s - Sunday %s, %s)"
+                         (format-time-string "Week %W")
+                         (format-time-string "%b %e" thisMonday)
+                         (format-time-string "%b %e" thisSunday)
+                         (format-time-string "%Y" thisMonday))))
+    (save-match-data
+      (goto-char (point-min))
+      (unless (re-search-forward heading nil 'no-error)
+        (end-of-line)
+        (newline)
+        (insert heading))
+      (unless (re-search-forward heading2 nil 'no-error)
+        (end-of-line)
+        (newline)
+        (insert heading2))
+      (org-end-of-subtree))))
+
+(setq org-default-notes-file (concat org-directory "notes.org"))
+(add-to-list 'org-capture-templates
+      `("i" "Internship Thoughts" plain (file+function ,(concat org-directory "/servicenow_2022s.org") yl/find-create-date-entry)
+         nil :unnarrowed t :empty-lines 1))
+
+;;
+;; Dired
+;;
+
+(evil-define-key 'normal dired-mode-map
+  "+" nil
+  "+f" 'dired-create-empty-file
+  "+d" 'dired-create-directory)
---
+++ doom/packages.el
@@ -0,0 +1,5 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+(package! olivetti)
+(package! org-roam)
