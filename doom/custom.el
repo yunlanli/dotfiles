d47ef9db81c56f17fd877bd4c13c48bf180719b8
yunlanli <chnsidney@gmail.com>
2022-10-29 02:05:20 -0400

    [ emacs ] variable font size for org headings

--- doom/custom.el
+++ doom/custom.el
@@ -59,5 +59,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "CaskaydiaCove NF" :foundry "nil" :slant normal :weight normal :height 140 :width normal)))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.4))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
 (put 'customize-browse 'disabled nil)
